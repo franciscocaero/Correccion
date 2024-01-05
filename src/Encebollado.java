2dd2041357202aa63d5f50ca294766896d7ddcf4
franciscocaero <150805792+franciscocaero@users.noreply.github.com>
2024-01-05 12:36:37 -0500

    Update Encebollado.java

--- src/Encebollado.java
+++ src/Encebollado.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Encebollado {
+public class Encebollado extends ComidaTipica{
+    double calificacion;
+    public Encebollado(String nombre, String region, double precio) {
+        super(nombre, region, precio);
+    }
+
+    public double getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(double calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    public void cali(){
+        System.out.println("Mi calificación del "+this.getNombre()+" es de: "+this.getCalificacion());
+    }
 }
