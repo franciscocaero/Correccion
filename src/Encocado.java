9e7f56578ff755be3568141efd02c5824f42a4ab
franciscocaero <150805792+franciscocaero@users.noreply.github.com>
2024-01-05 12:36:59 -0500

    Update Encocado.java

--- src/Encocado.java
+++ src/Encocado.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Encocado {
+public class Encocado extends ComidaTipica{
+
+    public Encocado(String nombre, String region, double precio) {
+        super(nombre, region, precio);
+    }
+    public void favor(){
+        System.out.println("\nMi plato favorito es el "+this.getNombre()+" Cuesta "+this.getPrecio());
+    }
 }
+
